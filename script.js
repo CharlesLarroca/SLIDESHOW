ccb585720474e349c93b6cbdf0ba367b10bee279
Charles Larroca <larroca_1000@hotmail.com>
2023-11-13 13:37:29 -0300

    Envio slideshow

---
+++ script.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const images = [
+  { "id": "1", "url": "./img/chrono.jpg" },
+  { "id": "2", "url": "./img/inuyasha.jpg" },
+  { "id": "3", "url": "./img/ippo.png" },
+  { "id": "4", "url": "./img/tenchi.jpg" },
+  { "id": "5", "url": "./img/tenjhotenge.jpg" },
+  { "id": "6", "url": "./img/yuyuhakusho.jpg" },
+];
+
+const containerItems = document.querySelector("#container-items");
+
+const loadImages = (images, containerItems) => {
+  images.forEach((img) => {
+    containerItems.innerHTML += `
+    <div class='items'>
+      <img src='${img.url}'>
+    </div>
+    `;
+  });
+};
+
+loadImages(images, containerItems);
+
+let items = document.querySelectorAll('.items')
+
+const next = () => {
+  containerItems.appendChild(items[0])
+  items = document.querySelectorAll('.items')
+}
+
+const previous = () => {
+  const lastItem = items[items.length - 1]
+  containerItems.insertBefore( lastItem, items[0])
+  items = document.querySelectorAll('.items')
+}
+
+document.querySelector('#previous').addEventListener('click', previous)
+document.querySelector('#next').addEventListener('click', next)
